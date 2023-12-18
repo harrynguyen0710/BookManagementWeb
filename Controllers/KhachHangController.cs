969b46d8357d2491d40e2da109e8512c7a919a24
Puppy <phunn280604@gmail.com>
2023-12-18 22:52:02 +0700

    Khách hàng

---
+++ Controllers/KhachHangController.cs
@@ -0,0 +1,30 @@
+﻿using BookManagementWeb.Data;
+using BookManagementWeb.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagementWeb.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly BookstoreDbContext _Context;
+
+        public KhachHangController(BookstoreDbContext context)
+        {
+            _Context = context;
+        }
+        public IActionResult Index()
+        {
+            List<KhachHang> khachHang;
+            khachHang = _Context.KHACHHANG.ToList();
+            return View(khachHang);
+        }
+
+        [HttpPost]
+        public IActionResult Create(KhachHang khachHang)
+        {
+            _Context.Add(khachHang);
+            _Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
